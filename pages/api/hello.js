f80d06e88a65efa69cdb7738a950c05341da6cc6
leonardoprimieri <leonardoprimieri>
2021-05-21 13:05:24 -0300

    .

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
+};
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('post', req)
+  } else {
+    // Handle any other HTTP method
+  }
 }
